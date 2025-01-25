ecfae4e92a7bc7b615a062b739109287f71427ce
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-01-25 15:45:50 +0300

    third commit

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9@27d%-&p0u^#p_n)!)r5g09h4#jsc5287y)mx&=7^!c2dm@ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'photogallery.urls'
