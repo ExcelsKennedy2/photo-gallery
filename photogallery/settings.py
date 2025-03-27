6203da0ac576225bc45e4f0ee7a7cab6c146caf3
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-03-27 15:01:34 +0300

    configured database

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,17 @@ WSGI_APPLICATION = 'photogallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://photo%20gallery_owner:npg_hYHEGx29rVkq@ep-weathered-salad-a59ghkm4-pooler.us-east-2.aws.neon.tech/photo%20gallery?sslmode=require"
+    )
 }
 
 
